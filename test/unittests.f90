bd7cd9d11a899cf25fcf85a8751472ad7d1f0cf0
Dalon Work <dwwork@gmail.com>
2021-10-27 20:40:36 -0400

    Added more unit tests

--- test/unittests.f90
+++ test/unittests.f90
@@ -11,6 +11,8 @@ call assign_dual
 call comparison_operators
 call addition
 call subtraction
+call multiplication
+call division
 
 contains
 
@@ -21,7 +23,6 @@ subroutine check(condition)
       write(*,*) test_name, " failed!"
         stop -1
     end if
-
 end subroutine
 
 subroutine assign_dual()
@@ -225,6 +226,122 @@ subroutine subtraction()
     call check(c%dx(3) == 0.0)
     call check(c%dx(4) == 0.0)
   end block unary_minus
+end subroutine
+
+subroutine multiplication()
+  type(dual) :: a, b
+  integer :: i
+
+  a = 2
+  b = 3
+
+  a%dx(1) = 1.0
+  b%dx(2) = 1.0
+
+  mul_duals: block
+    type(dual) :: c
+    test_name = "mul_duals"
+    c = a * b
+    call check(c == 6.0)
+    call check(c%dx(1) == 3.0)
+    call check(c%dx(2) == 2.0)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+  end block mul_duals
+
+  mul_real: block
+    type(dual) :: c
+    test_name = "mul_real"
+    c = a * 1.5
+    call check(c == 3.0)
+    call check(c%dx(1) == 1.5)
+    call check(c%dx(2) == 0.0)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+
+    c = 10.5 * b
+    call check(c == 31.5)
+    call check(c%dx(1) == 0.0)
+    call check(c%dx(2) == 10.5)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+  end block mul_real
+
+  mul_integer: block
+    type(dual) :: c
+    test_name = "mul_integer"
+    c = a * 20
+    call check(c == 40)
+    call check(c%dx(1) == 20.0)
+    call check(c%dx(2) == 0.0)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+
+    c = 10 * b
+    call check(c == 30)
+    call check(c%dx(1) == 0.0)
+    call check(c%dx(2) == 10.0)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+  end block mul_integer
+end subroutine
+
+subroutine division()
+  type(dual) :: a, b
+  integer :: i
+
+  a = 2
+  b = 4
+
+  a%dx(1) = 1.0
+  b%dx(2) = 1.0
+
+  div_duals: block
+    type(dual) :: c
+    test_name = "div_duals"
+    c = a / b
+    call check(c == 0.5)
+    call check(c%dx(1) == 0.25)
+    call check(c%dx(2) == -0.125)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+  end block div_duals
+
+  div_real: block
+    type(dual) :: c
+    test_name = "div_real"
+    c = a / 0.5
+    call check(c == 4.0)
+    call check(c%dx(1) == 2.0)
+    call check(c%dx(2) == 0.0)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+
+    c = 16.0 / b
+    call check(c == 4.0)
+    call check(c%dx(1) == 0.0)
+    call check(c%dx(2) == -1.0)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+  end block div_real
+
+  div_integer: block
+    type(dual) :: c
+    test_name = "div_integer"
+    c = a / 4
+    call check(c == 0.5)
+    call check(c%dx(1) == 0.25)
+    call check(c%dx(2) == 0.0)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+
+    c = 40 / b
+    call check(c == 10)
+    call check(c%dx(1) == 0.0)
+    call check(c%dx(2) == -2.5)
+    call check(c%dx(3) == 0.0)
+    call check(c%dx(4) == 0.0)
+  end block div_integer
 end subroutine
 
 end program unittests
